d525b6a3c71b0efde5b720f4928ab755ba6a2003
Gracey Wilson <gracey.wilson@students.olin.edu>
2017-10-21 01:45:08 -0400

    backing up

---
+++ API.py
@@ -0,0 +1,2 @@
+UID = 7286
+API_key = b601cddd5a9e0506629782d3bd2869ea
---
+++ reversesearch.php
@@ -0,0 +1,53 @@
+// signing in and verifying
+
+<?php
+$data = array("uid"=>7286,"expires"=>1508561461,"auth"=>"73edd0303dc431d5ab8b9dcc4a380d85");
+$json = json_encode($data);
+$context = stream_context_create(array(
+ 'http' => array(
+   "method" => "POST",
+   "Content-Type: application/json\r\n",
+   'content' => $json
+ 	)
+ );
+$response = file_get_contents($endpoint, FALSE, $context);
+
+$uid = 7286;
+$apikey = "b601cddd5a9e0506629782d3bd2869ea";
+$expires = time() + 300; //
+
+"auth":"34ec82c51d90f3227a70a105e477960a"
+
+$uid = 1;
+$apikey = "";
+$expires = time()+300; // Expires - when the signature will become invalid (UNIX timestamp) - may be no more than 1200 seconds from now.
+
+// Generate auth
+$stringToHash = $uid."-".$expires."-".$apikey;
+$auth = md5($stringToHash);
+
+$data = array("uid"=>$uid,"expires"=>$expires,"auth"=>$auth);
+?>
+
+// Check images using Incandescent API_key
+
+<?php
+// assumes there is only one image in the directory with a .jpg extension
+$directory = "/Documents/MVP/images";
+
+// $images = array("http://www.domain.com/images/1.jpg","http://www.domain.com/images/2.jpg");
+$images = glob($directory , "*.jpg"); // returns an array
+
+$data = array("uid"=>$uid,"expires"=>$expires,"auth"=>$auth,"images"=>$images,"multiple"=>5);
+$json = json_encode($data);
+$context = stream_context_create(array(
+		'http' => array(
+			'method' => 'POST',
+			"Content-Type: application/json\r\n",
+			'content' => $json
+		)
+	));
+$response = file_get_contents("https://incandescent.xyz/api/add/", FALSE, $context);
+?>
+
+// have to wait at least 5 seconds before calling this
